ce4292ea11bdfe2c212870b52db9bed882773d01
samueldlh <samueldlh.dev@gmail.com>
2022-02-19 00:23:58 -0500

    use pinia

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,5 +1,21 @@
 <template>
-   <div>
-      Counter View 
-   </div>
+  <h3>Counter View: {{ counterStore.counter }}</h3>
+  <button @click="counterStore.increment(1)">+1</button>
+  <button @click="counterStore.increment(3)">+3</button>
+  <button @click="counterStore.increment(5)">+5</button>
 </template>
+
+<script lang="ts" setup>
+import { useCounterStore } from "../stores/counter";
+const counterStore = useCounterStore();
+</script>
+
+<style>
+button{
+  margin: 0.3rem;
+  border-radius: 0.5rem;
+  width: 3rem;
+  height: 1.9rem;
+  border: none;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
-   <div>
-      Home View 
-   </div>
+  <h3>Home View: {{ counterStore.counter }}</h3>
+  <h3>Numero de cambios: {{ counterStore.changes }}</h3>
 </template>
+
+<script lang="ts" setup>
+import { useCounterStore } from "../stores/counter";
+const counterStore = useCounterStore();
+</script>
