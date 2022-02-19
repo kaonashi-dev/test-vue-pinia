03c95c037064e419c6f9364f6c94bd3a71105fa9
samueldlh <samueldlh.dev@gmail.com>
2022-02-18 19:45:10 -0500

    config router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	
+	<h1>APP</h1>
+
+	<router-link to="/home">Home</router-link>
+	-
+	<router-link to="/counter">Counter</router-link>
+
+	<router-view></router-view>
+
 </template>
 
 <style>
